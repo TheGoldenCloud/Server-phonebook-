80306db0edf924ecf6d40e732df3a13e4f1ea6e2
GoldenCloudPc <jvladimir54@yahoo.com>
2023-05-03 13:58:00 +0200

    modifyed jwt

--- routes/userRouts.js
+++ routes/userRouts.js
@@ -1,11 +1,11 @@
 import express from "express"
-import { getUser, createUser,getUserById,updateUser,deleteUser,loginUser } from "../controllers/userController.js";
+import { getUser, createUser,getUserById,updateUser,deleteUser,loginUser,verifyToken } from "../controllers/userController.js";
 
 let rout = express.Router();
 
 rout.route('/users/login').post(loginUser);
 
-rout.route('/users').get(getUser).post(createUser);
+rout.route('/users').get(verifyToken).get(getUser).post(createUser);
 
 rout.route('/users/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
